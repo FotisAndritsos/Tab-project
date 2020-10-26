bab24516d44f6adf4f6e7638145522ac4fc5e7d1
FotisAndritsos <fotisandritsos85@gmail.com>
2020-10-26 23:49:35 +0200

    final and media queries

--- main.js
+++ main.js
@@ -2,29 +2,35 @@ const btns = document.querySelectorAll('button');
 const img1 = document.querySelector('.img1');
 const img2 = document.querySelector('.img2');
 const img3 = document.querySelector('.img3');
-
+const btnOne = document.getElementById('btnOne');
+const btnTwo = document.getElementById('btnTwo');
+const btnThree = document.getElementById('btnThree');
 
 btns.forEach(btn => {
 
    btn.addEventListener('click', event => {
         if( event.target.id === "btnOne" ){
             event.target.classList.add('active');
+            btnTwo.classList.remove('active');
+            btnThree.classList.remove('active');
             img1.style.display = "block";
             img2.style.display = "none";
             img3.style.display = "none";
         }else if(event.target.id === "btnTwo"){
             event.target.classList.add('active');
+            btnOne.classList.remove('active');
+            btnThree.classList.remove('active');
             img1.style.display = "none";
             img2.style.display = "block";
             img3.style.display = "none";
         }else if(event.target.id === "btnThree"){
             event.target.classList.add('active');
+            btnTwo.classList.remove('active');
+            btnOne.classList.remove('active');
             img1.style.display = "none";
             img2.style.display = "none";
             img3.style.display = "block";
-        }
-           
-        
+        }     
    });
 
 })
